48fca55d53b0178e5345e6d09b1cad8524d85385
khl <khl@khl-laptop>
2016-08-26 23:32:58 +0200

    Added script to concatenate files into one cpp

---
+++ produce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE_LIST="\
+	src/Coordinates.hpp \
+	src/Command.hpp \
+	src/Checkpoint.hpp \
+	src/Player.hpp \
+	src/Params.hpp \
+	src/Logic.hpp \
+	src/IO.hpp \
+	src/Game.hpp \
+	src/main.cpp \
+"
+
+grep -h "#include <" ${FILE_LIST} | sort -u
+
+for FILE in ${FILE_LIST};
+do
+	grep -v "#include" ${FILE} | grep -v "#pragma once";
+done
